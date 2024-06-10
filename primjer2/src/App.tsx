4f7de88051801d77926edb51cc2df092392af801
tintort <tamara.tintor@lanaco.com>
2024-06-10 13:44:36 +0200

    Update App.tsx

--- primjer2/src/App.tsx
+++ primjer2/src/App.tsx
@@ -2,17 +2,7 @@ import './App.css';
 import UserForm from './UseForm';
 import { UserProvider } from './UserContext';
 import UserView from './UserView';
-/*
-interface UserContextType {
-  user: IUser;
-  dispatch: React.Dispatch<Actions>;
-}
-export interface IUser {
-  name: string;
-  age: number;
-}
-export const UserContext = createContext<UserContextType | undefined>(undefined);
-*/
+
 const App = () => {
   return (
     <UserProvider>
