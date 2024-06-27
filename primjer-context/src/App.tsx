fb19e7235554c3375514b4d2d6c971ddfdbe3514
tintort <tamara.tintor@lanaco.com>
2024-06-27 10:09:05 +0200

    laksi nacin definisanja UserContext-a

--- primjer-context/src/App.tsx
+++ primjer-context/src/App.tsx
@@ -1,16 +1,28 @@
+import { createContext, useReducer } from 'react';
 import './App.css';
-import { UserProvider } from './UserContext';
 import UserForm from './UserForm';
 import UserView from './UserView';
+import { Actions } from './actions/actions';
+import userReducer from './reducer/reducer';
+import { IUser, initialState } from './state/userState';
+
+export const UserContext = createContext<{
+  user: IUser;
+  dispatch: React.Dispatch<Actions>;
+}>({
+  user: initialState,
+  dispatch: () => undefined,
+});
 
 const App = () => {
+  const [user, dispatch] = useReducer(userReducer, initialState);
   return (
-    <UserProvider>
+    <UserContext.Provider value={{ user, dispatch }}>
       <div className='App'>
         <UserForm />
         <UserView />
       </div>
-    </UserProvider>
+    </UserContext.Provider>
   );
 };
 
